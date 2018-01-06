ca7bfeb3f790fa04d5b571d6df263323e03a203a
carlesba <carlesba.mail@gmail.com>
2018-01-06 14:54:47 +0100

    chore: fix logs when tooling

--- dist/immootable.esm.js
+++ dist/immootable.esm.js
@@ -1,4 +1,77 @@
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+var defineProperty = function (obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+};
+
+var _extends = Object.assign || function (target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i];
+
+    for (var key in source) {
+      if (Object.prototype.hasOwnProperty.call(source, key)) {
+        target[key] = source[key];
+      }
+    }
+  }
+
+  return target;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var toArray = function (arr) {
+  return Array.isArray(arr) ? arr : Array.from(arr);
+};
+
+var toConsumableArray = function (arr) {
+  if (Array.isArray(arr)) {
+    for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) arr2[i] = arr[i];
+
+    return arr2;
+  } else {
+    return Array.from(arr);
+  }
+};
 
 var curried = function curried(callback, args, arity) {
   return arity === 0 ? callback.apply(undefined, args) : function () {
@@ -6,7 +79,7 @@ var curried = function curried(callback, args, arity) {
       newArgs[_key] = arguments[_key];
     }
 
-    return curried(callback, [].concat(_toConsumableArray(args), newArgs), arity - newArgs.length);
+    return curried(callback, [].concat(toConsumableArray(args), newArgs), arity - newArgs.length);
   };
 };
 
@@ -38,10 +111,8 @@ var pipe = function pipe() {
   };
 };
 
-function _toConsumableArray$1(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
-
 var reverse = function reverse(list) {
-  return [].concat(_toConsumableArray$1(list)).reverse();
+  return [].concat(toConsumableArray(list)).reverse();
 };
 
 var _combine = pipe(reverse, apply$1(pipe));
@@ -82,18 +153,16 @@ var keyPath = function keyPath(source) {
   return Array.isArray(source) ? source : [source];
 };
 
-var get = function get(key, source) {
+var get$1 = function get(key, source) {
   return keyPath(key).reduce(function (value, key) {
     return has$1(key, value) ? value[key] : undefined;
   }, source);
 };
 
-var get$1 = curry(get);
-
-function _toConsumableArray$2(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+var get$2 = curry(get$1);
 
 var insert = function insert(index, value, list) {
-  return [].concat(_toConsumableArray$2(list.slice(0, index)), [value], _toConsumableArray$2(list.slice(index, list.length)));
+  return [].concat(toConsumableArray(list.slice(0, index)), [value], toConsumableArray(list.slice(index, list.length)));
 };
 
 var insert$1 = curry(insert);
@@ -143,44 +212,36 @@ var prepend = function prepend(value, list) {
 
 var prepend$1 = curry(prepend);
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr); }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
 var _set = function _set(key, value, source) {
   return Array.isArray(source) ? source.map(function (v, i) {
     return i === key ? value : v;
-  }) : _extends({}, source, _defineProperty({}, key, value));
+  }) : _extends({}, source, defineProperty({}, key, value));
 };
 
-var set = function set(key, value, source) {
+var set$1 = function set$$1(key, value, source) {
   var keys = keyPath(key);
 
-  var _keys = _toArray(keys),
+  var _keys = toArray(keys),
       nextKey = _keys[0],
       rest = _keys.slice(1);
 
-  var nextValue = rest.length === 0 ? value : set(rest, value, get$1(nextKey, source));
+  var nextValue = rest.length === 0 ? value : set$$1(rest, value, get$2(nextKey, source));
   return _set(nextKey, nextValue, source);
 };
 
-var set$1 = curry(set);
-
-function _toArray$1(arr) { return Array.isArray(arr) ? arr : Array.from(arr); }
+var set$2 = curry(set$1);
 
 var update = function update(key, callback, source) {
   var keys = keyPath(key);
 
-  var _keys = _toArray$1(keys),
+  var _keys = toArray(keys),
       nextKey = _keys[0],
       rest = _keys.slice(1);
 
   var value = rest.length ? update(rest, callback, source[nextKey]) : callback(source[nextKey]);
-  return set$1(nextKey, value, source);
+  return set$2(nextKey, value, source);
 };
 
 var update$1 = curry(update);
 
-export { append$1 as append, apply$1 as apply, combine, curry, filter$1 as filter, find$1 as find, defaultTo$1 as defaultTo, get$1 as get, has$1 as has, insert$1 as insert, keyPath, log$1 as log, map$1 as map, omit$1 as omit, passBy$1 as passBy, prepend$1 as prepend, pipe, reverse, set$1 as set, update$1 as update };
+export { append$1 as append, apply$1 as apply, combine, curry, filter$1 as filter, find$1 as find, defaultTo$1 as defaultTo, get$2 as get, has$1 as has, insert$1 as insert, keyPath, log$1 as log, map$1 as map, omit$1 as omit, passBy$1 as passBy, prepend$1 as prepend, pipe, reverse, set$2 as set, update$1 as update };
--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,11 @@ import pkg from './package.json'
 
 export default [
   {
-    entry: 'src/index.js',
-    name: 'immootable',
-    targets: [
-      { dest: pkg.main, format: 'cjs' },
-      { dest: pkg.module, format: 'es' },
-      { dest: pkg.browser, format: 'umd' }
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs', name: 'immootable.cjs' },
+      { file: pkg.module, format: 'es', name: 'immootable.es' },
+      { file: pkg.browser, format: 'umd', name: 'immootable.umd' }
     ],
     plugins: [
       babel({
